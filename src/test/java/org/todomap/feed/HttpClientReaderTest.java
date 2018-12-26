d345b3054a364203c09424faa299577776357ccb
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2018-12-26 23:09:08 +0100

    some tests disabled
    referring to outdated urls
    
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/test/java/org/todomap/feed/HttpClientReaderTest.java
+++ src/test/java/org/todomap/feed/HttpClientReaderTest.java
@@ -27,13 +27,6 @@ public class HttpClientReaderTest {
 	}
 	
 	@Test
-	public void testReadWithGzip() throws IOException {
-		NewsFeed feed = HttpClientReader.read("http://blog.dictat.org/?feed=rss2");
-		Assert.assertNotNull(feed);
-		Assert.assertTrue(feed.isTransportCompressed());
-	}
-
-	@Test
 	public void testReread() throws IOException {
 		NewsFeed feed = HttpClientReader.read("http://iwillworkforfood.blogspot.com/feeds/posts/default");
 		Assert.assertNotNull(feed);
